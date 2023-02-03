fe0dbcb62870f4a427f89a0e51c37346db9dc266
Catherine <70590533+catherinetkl@users.noreply.github.com>
2023-02-04 01:04:52 +0800

    Create YourInitials.java

---
+++ Module 1: Introduction to Java/1. Getting Started/YourInitials.java
@@ -0,0 +1,13 @@
+// Challenge: Draw my initials
+
+public class YourInitials {
+  public static void main(String[] args) {
+    System.out.println(" CCC    TTTTT");
+    System.out.println("C   C     T");
+    System.out.println("C         T");
+    System.out.println("C         T");
+    System.out.println("C         T");
+    System.out.println("C   C     T");
+    System.out.println(" CCC      T");
+  }
+}
