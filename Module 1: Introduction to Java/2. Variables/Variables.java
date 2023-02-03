de76ac5dbc07545e0b39bd47d545069686511870
Catherine <70590533+catherinetkl@users.noreply.github.com>
2023-02-04 01:35:49 +0800

    Create Variables.java

---
+++ Module 1: Introduction to Java/2. Variables/Variables.java
@@ -0,0 +1,18 @@
+// Identify a type that can store bus passengers
+// 1. Int (Golden Rule: Use the data type with the smallest memory capacity)
+
+// Name the variable using lowerCamelCase e.g. name, phoneNumber, dateOfBirth
+// int passengers
+
+// Set variable equal to a value
+// int passengers = 10;
+
+public class Variables {
+  public static void main(String[] args) {
+    int passengers = 10;
+    System.out.println(passengers);
+
+    int busTickets = passengers;
+    System.out.println(busTickets);
+  }
+}
