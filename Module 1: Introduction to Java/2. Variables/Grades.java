1c0e679c45ea9f5c5a7ac184fd772d09605e59d2
Catherine <70590533+catherinetkl@users.noreply.github.com>
2023-02-04 16:15:18 +0800

    Create Grades.java

---
+++ Module 1: Introduction to Java/2. Variables/Grades.java
@@ -0,0 +1,13 @@
+// char type is used to store single characters, only use 2 bytes of memory
+// Task 1: Create a method that displays the grades for magic subjects taught in Hogwarts.
+public class Grades {
+  public static void main(String[] args) {
+    char potions = 'A';
+    char arithmancy = 'B';
+    char charms = 'C';
+
+    System.out.println("Potions " + potions);
+    System.out.println("Arithmancy " + arithmancy);
+    System.out.println("Charms " + charms);
+  }
+}
