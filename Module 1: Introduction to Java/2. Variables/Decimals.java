eeae8e7436af198d583965365ffcac12e015a38c
Catherine <70590533+catherinetkl@users.noreply.github.com>
2023-02-06 18:12:16 +0800

    Create Decimals.java

---
+++ Module 1: Introduction to Java/2. Variables/Decimals.java
@@ -0,0 +1,13 @@
+public class Decimals {
+  public static void main(String[] args) {
+    double percentage = 73.5;
+    System.out.println(percentage + " of percentages are made up");
+
+    double wallet = 20; // always use double for math calculation because in division,
+    //using int, java remove decimal place after whole number
+
+    int people = 3;
+    System.out.println(wallet / people);
+
+  }
+}
