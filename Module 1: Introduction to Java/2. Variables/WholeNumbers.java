e9764cd554a540623ef4d6ed3b2b8fc7275924a0
Catherine <70590533+catherinetkl@users.noreply.github.com>
2023-02-06 17:56:13 +0800

    Create WholeNumbers.java

---
+++ Module 1: Introduction to Java/2. Variables/WholeNumbers.java
@@ -0,0 +1,11 @@
+// use long to store very large whole numbers
+
+public class WholeNumbers {
+  public static void main(String[] args) {
+    long globalPopulation = 8_000_000_000L;
+    System.out.println("The global population is " + globalPopulation);
+
+    long dailyGoogleSearches = 8_500_000_000L;
+    System.out.println("Daily, there are " + dailyGoogleSearches + " google searches");
+  }
+}
