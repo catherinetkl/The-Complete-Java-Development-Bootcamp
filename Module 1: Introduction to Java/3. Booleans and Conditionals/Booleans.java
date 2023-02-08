d2dbbc5291a53c5889085eb03913679af4b30bb9
Catherine <70590533+catherinetkl@users.noreply.github.com>
2023-02-08 17:22:01 +0800

    Create Booleans.java

---
+++ Module 1: Introduction to Java/3. Booleans and Conditionals/Booleans.java
@@ -0,0 +1,13 @@
+// use conditions to control which part of your code runs
+// set bool1 as variable for true
+// set bool2 as variable for false
+
+public class Booleans {
+  public static void main(String[] args) {
+    boolean bool1 = true;
+    boolean bool2 = false;
+
+    System.out.println(bool1);
+    System.out.println(bool2);
+  }
+}
