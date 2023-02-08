cdfd44590ab2187cf7b70c98f6205124580edd5b
Catherine <70590533+catherinetkl@users.noreply.github.com>
2023-02-08 17:22:20 +0800

    Create BooleanComparisons.java

---
+++ Module 1: Introduction to Java/3. Booleans and Conditionals/BooleanComparisons.java
@@ -0,0 +1,19 @@
+public class BooleanComparisons {
+  public static void main(String[] args) {
+    int chemistryGrade = 95;
+    int biologyGrade = 75;
+    int englishGrade = 75;
+
+    System.out.println(biologyGrade < chemistryGrade);
+    System.out.println(chemistryGrade >= englishGrade);
+
+    String sentence = "I love this course!";
+    String sentence2 = "I love this course!";
+    System.out.println(sentence.equals(sentence2));
+    System.out.println(!sentence.equals(sentence2));
+
+    double sales = 37.55;
+    double costs = 5.55;
+    System.out.println(sales > costs);
+  }
+}
